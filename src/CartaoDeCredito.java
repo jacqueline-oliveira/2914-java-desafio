3ac6a74f15659c460e5bb7d20b8acd5417c35a4e
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-03-21 15:26:25 -0300

    vídeo 5.2

---
+++ src/CartaoDeCredito.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public boolean lancaCompra(Compra compra) {
+        if(this.saldo > compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+
+        return false;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+}
---
+++ src/Compra.java
@@ -0,0 +1,23 @@
+public class Compra {
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: descricao = " + descricao +
+                " valor =" + valor;
+    }
+}
