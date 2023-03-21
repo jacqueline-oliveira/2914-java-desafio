108c5571a834e4cb3058cd1ba829e96fb0c98a78
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-03-21 15:33:51 -0300

    vídeo 5.3

--- src/Compra.java
+++ src/Compra.java
@@ -1,4 +1,4 @@
-public class Compra {
+public class Compra implements Comparable<Compra> {
     private String descricao;
     private double valor;
 
@@ -19,5 +19,10 @@ public class Compra {
     public String toString() {
         return "Compra: descricao = " + descricao +
                 " valor =" + valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
     }
 }
---
+++ src/Principal.java
@@ -0,0 +1,44 @@
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o limite do cartão: ");
+        double limite = leitura.nextDouble();
+        CartaoDeCredito cartao = new CartaoDeCredito(limite);
+
+        int sair = 1;
+        while(sair != 0) {
+            System.out.println("Digite a descrição da compra:");
+            String descricao = leitura.next();
+
+            System.out.println("Digite o valor da compra:");
+            double valor = leitura.nextDouble();
+
+            Compra compra = new Compra(descricao, valor);
+            boolean compraRealizada = cartao.lancaCompra(compra);
+
+            if (compraRealizada) {
+                System.out.println("Compra realizada!");
+                System.out.println("Digite 0 para sair ou 1 para continuar");
+                sair = leitura.nextInt();
+            } else {
+                System.out.println("Saldo insuficiente!");
+                sair = 0;
+            }
+        }
+
+        System.out.println("***********************");
+        System.out.println("COMPRAS REALIZADAS:\n");
+        Collections.sort(cartao.getCompras());
+        for (Compra c : cartao.getCompras()) {
+            System.out.println(c.getDescricao() + " - " +c.getValor());
+        }
+        System.out.println("\n***********************");
+
+        System.out.println("\nSaldo do cartão: " +cartao.getSaldo());
+    }
+
+
+}
